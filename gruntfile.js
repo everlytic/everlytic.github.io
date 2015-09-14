357090d9f1a33c8d0c12a57d60d4c3d1a9ed413e
Charl Kruger <nit3watch@gmail.com>
2015-09-14 14:00:27 +0200

    minified images

--- gruntfile.js
+++ gruntfile.js
@@ -13,11 +13,30 @@ module.exports = function(grunt) {
                 ],
                 dest: '_sass/'
             }
+        },
+        uncss: {
+            dist: {
+                files: {
+                    'dist/css/tidy.css': ['index.html', 'about.html']
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'images/',                   // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'images/'                  // Destination path prefix
+                }]
+            }
         }
     });
 
     // Plugins
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-uncss');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
     // Tasks
     grunt.registerTask('default', ['copy']);
